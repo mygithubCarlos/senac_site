1063a9f5caa95d8aa673303adc9bf76ca8a36ce2
Carlos Silva <meuscursosdeprogramacao@hotmail.com>
2026-01-28 20:51:41 -0300

    atualização do trabalho (trabalho 2)

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,10 @@
 # pages/urls.py
 from django.urls import path
-from .views import home
+from .views import home, sobre, contato, ajuda
 
 urlpatterns = [
     path('', home, name='home'),
+    path("sobre/", sobre, name='sobre'),
+    path('contato/', contato, name='contato'),
+    path('ajuda/', ajuda, name='ajuda')
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,12 @@ def home(request):
     }
     return render(request, 'pages/home.html', contexto)
 
+def sobre(request):
+    return render(request, "pages/sobre.html")
+
+def contato(request):
+    return render(request, "pages/contato.html")
+
+def ajuda(request):
+    return render(request, "pages/ajuda.html")
+
